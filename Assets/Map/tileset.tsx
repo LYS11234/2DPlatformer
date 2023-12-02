de687ec172138cd788109253929bac7d379e6d05
LYS11234 <young990420@gmail.com>
2023-12-03 02:07:24 +0900

    대용량 분할 업로드

---
+++ Assets/Map/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tileset" tilewidth="32" tileheight="32" tilecount="30" columns="10">
+ <image source="../Gothicvania Swamp Artwork/Evironment/tileset.png" width="336" height="112"/>
+</tileset>
